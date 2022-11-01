c3e660daa9604238025d1fcf78f3a2dc99b0a2ce
yalfarosullivan@yahoo.com <Yara Alfaro-Sullivan>
2022-11-01 08:16:34 -0700

    added functional timer

--- timer.js
+++ timer.js
@@ -1,32 +1,15 @@
 //Create a timer
-let remainingTimeElement = document.querySelector("#remainingTime"),
-             secondsLeft = 10,
-             progressBar = document.querySelector("#progressBar"),
-          progressBarDiv = document.querySelector(".progressBarDiv"),
-          downloadBegins = document.querySelector(".downloadBegins"),
-        downloadDuration = 10
+var counter = 10;
 
-const downloadTimer = setInterval( 
-    () => {
-        if (secondsLeft <= 0) {
-            clearInterval(downloadTimer)
-            downloadBegins.style.display = "None"
-        }
-        remainingTimeElement.value = secondsLeft
-        remainingTimeElement.textContent = secondsLeft
-        secondsLeft -= 1
-    }, 
-1000)
+setInterval( function(){
+    counter--;
 
-setTimeout(() => {
-    const downloadProgress = setInterval( 
-        () => {
-            if (downloadDuration <= 0) clearInterval(downloadProgress)
-            progressBar.value = 10 - downloadDuration
-            progressBar.textContent = `${(10 - downloadDuration) * 10}%`
-            progressBarDiv.style.width = `${(10 - downloadDuration) * 10}%`
-            progressBarDiv.style.padding = `${0.5}rem`
-            downloadDuration -= 1
-        }, 
-    100)
-}, 12000)
+    if(counter >=0){
+        id = document.getElementById("count")
+        id.innerHTML = counter;
+    }
+
+    if (counter === 0){
+        id.innerHTML= "FINISH";
+    }
+}, 1000);
