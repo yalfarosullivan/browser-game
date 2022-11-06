488887e23bc5f7d871ba634987fce65d0bb49156
yalfarosullivan@yahoo.com <Yara Alfaro-Sullivan>
2022-11-06 07:43:11 -0800

    added code for cards and images

--- roadBlock.js
+++
@@ -1,8 +0,0 @@
-
-function createImage(url){
-    let image = document.createElement('img')
-    image.src = url
-    image.style.position = 'absolute'
-    document.body.append(image)
-    return image
-}
